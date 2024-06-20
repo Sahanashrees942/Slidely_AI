a34eb0303bc5e0388ab5d094229d77a177e4247c
Sahanashrees942 <sahanashrees83@gmail.com>
2024-06-20 19:17:15 +0530

    Add files via upload

---
+++ ViewSubmissionsForm.Designer.vb
@@ -0,0 +1,189 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class ViewSubmissionsForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblEmail = New System.Windows.Forms.Label()
+        Me.lblPhone = New System.Windows.Forms.Label()
+        Me.lblGitHubLink = New System.Windows.Forms.Label()
+        Me.lblStopwatch = New System.Windows.Forms.Label()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.btnPrevious = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(326, 44)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(44, 16)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Name"
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Location = New System.Drawing.Point(326, 84)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(41, 16)
+        Me.lblEmail.TabIndex = 1
+        Me.lblEmail.Text = "Email"
+        '
+        'lblPhone
+        '
+        Me.lblPhone.AutoSize = True
+        Me.lblPhone.Location = New System.Drawing.Point(326, 121)
+        Me.lblPhone.Name = "lblPhone"
+        Me.lblPhone.Size = New System.Drawing.Size(46, 16)
+        Me.lblPhone.TabIndex = 2
+        Me.lblPhone.Text = "Phone"
+        '
+        'lblGitHubLink
+        '
+        Me.lblGitHubLink.AutoSize = True
+        Me.lblGitHubLink.Location = New System.Drawing.Point(326, 161)
+        Me.lblGitHubLink.Name = "lblGitHubLink"
+        Me.lblGitHubLink.Size = New System.Drawing.Size(75, 16)
+        Me.lblGitHubLink.TabIndex = 3
+        Me.lblGitHubLink.Text = "GitHub Link"
+        '
+        'lblStopwatch
+        '
+        Me.lblStopwatch.AutoSize = True
+        Me.lblStopwatch.Location = New System.Drawing.Point(326, 202)
+        Me.lblStopwatch.Name = "lblStopwatch"
+        Me.lblStopwatch.Size = New System.Drawing.Size(103, 16)
+        Me.lblStopwatch.TabIndex = 4
+        Me.lblStopwatch.Text = "Stopwatch Time"
+        '
+        'btnNext
+        '
+        Me.btnNext.Location = New System.Drawing.Point(256, 291)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(173, 23)
+        Me.btnNext.TabIndex = 5
+        Me.btnNext.Text = "&Next (Press N)"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'btnPrevious
+        '
+        Me.btnPrevious.Location = New System.Drawing.Point(49, 291)
+        Me.btnPrevious.Name = "btnPrevious"
+        Me.btnPrevious.Size = New System.Drawing.Size(164, 23)
+        Me.btnPrevious.TabIndex = 6
+        Me.btnPrevious.Text = "&Previous (Press  P)"
+        Me.btnPrevious.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(49, 37)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(185, 22)
+        Me.TextBox1.TabIndex = 7
+        Me.TextBox1.Text = "Name"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(49, 78)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(185, 22)
+        Me.TextBox2.TabIndex = 8
+        Me.TextBox2.Text = "Email"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(49, 121)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(185, 22)
+        Me.TextBox3.TabIndex = 9
+        Me.TextBox3.Text = "Phone Number"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(49, 161)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(185, 22)
+        Me.TextBox4.TabIndex = 10
+        Me.TextBox4.Text = "GitHubLink For Slidely Task 2"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(49, 202)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(185, 22)
+        Me.TextBox5.TabIndex = 11
+        Me.TextBox5.Text = "StopWatch Time"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(49, 0)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(380, 22)
+        Me.TextBox6.TabIndex = 12
+        Me.TextBox6.Text = "Sahanashree S, Slidely Task 2"
+        '
+        'ViewSubmissionsForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnPrevious)
+        Me.Controls.Add(Me.btnNext)
+        Me.Controls.Add(Me.lblStopwatch)
+        Me.Controls.Add(Me.lblGitHubLink)
+        Me.Controls.Add(Me.lblPhone)
+        Me.Controls.Add(Me.lblEmail)
+        Me.Controls.Add(Me.lblName)
+        Me.Name = "ViewSubmissionsForm"
+        Me.Text = "ViewSubmissionsForm"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblName As Label
+    Friend WithEvents lblEmail As Label
+    Friend WithEvents lblPhone As Label
+    Friend WithEvents lblGitHubLink As Label
+    Friend WithEvents lblStopwatch As Label
+    Friend WithEvents btnNext As Button
+    Friend WithEvents btnPrevious As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+End Class
---
+++ ViewSubmissionsForm.vb
@@ -0,0 +1,50 @@
+﻿Imports System.Net.Http
+Imports Newtonsoft.Json
+
+Public Class ViewSubmissionsForm
+    Private httpClient As HttpClient = New HttpClient()
+    Private currentIndex As Integer = 0
+
+    Private Async Sub ViewSubmissionsForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Await LoadSubmission(currentIndex)
+    End Sub
+
+    Private Async Function LoadSubmission(index As Integer) As Task
+        Try
+            Dim response As HttpResponseMessage = Await httpClient.GetAsync($"http://localhost:3000/read?index={index}")
+
+            If response.IsSuccessStatusCode Then
+                Dim json As String = Await response.Content.ReadAsStringAsync()
+                Dim submission As Dictionary(Of String, String) = JsonConvert.DeserializeObject(Of Dictionary(Of String, String))(json)
+
+                If submission IsNot Nothing Then
+                    lblName.Text = submission("name")
+                    lblEmail.Text = submission("email")
+                    lblPhone.Text = submission("phone")
+                    lblGitHubLink.Text = submission("github_link")
+                    lblStopwatch.Text = submission("stopwatch_time")
+                End If
+            Else
+                MessageBox.Show("No more submissions.")
+            End If
+        Catch ex As Exception
+            MessageBox.Show($"Error loading submission: {ex.Message}")
+        End Try
+    End Function
+
+    Private Async Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+        currentIndex += 1
+        Await LoadSubmission(currentIndex)
+    End Sub
+
+    Private Async Sub btnPrevious_Click(sender As Object, e As EventArgs) Handles btnPrevious.Click
+        If currentIndex > 0 Then
+            currentIndex -= 1
+            Await LoadSubmission(currentIndex)
+        End If
+    End Sub
+
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles TextBox5.TextChanged
+
+    End Sub
+End Class
